d79ff447f968596beb76ac666e39b6d2ed337f14
xiaodong.li <xiaodong.li@asiainnovations.com>
2018-11-20 17:02:24 +0800

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,9 +25,9 @@ module.exports = {
       'path': '/var/www/production',
       'pre-setup': "yum install git -y;",
       'post-setup': "ls -la",
-      'pre-deploy-local': 'pwd;',
-      'pre-deploy': 'ls -la;',
-      'post-deploy': 'git pull origin master;npm run start_production;',
+      'pre-deploy-local': "pwd; echo 'this is a local command' ",
+      'pre-deploy': 'git pull origin master; git log --pretty=format:"%h %s %ad" -n 2 --graph;',
+      'post-deploy': 'npm run start_production;',
       'test': 'pm2 ls;'
     }
   }
